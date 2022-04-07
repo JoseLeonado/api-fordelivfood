b9da794de0d108402454c215577cc6a0aeb3ebcc
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2022-04-06 21:49:30 -0300

    Criando projeto Spring Boot

---
+++ src/main/java/com/jlcb/fordelivfood/ApiFordelivfoodApplication.java
@@ -0,0 +1,13 @@
+package com.jlcb.fordelivfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFordelivfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFordelivfoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jlcb/fordelivfood/ApiFordelivfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jlcb.fordelivfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFordelivfoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
