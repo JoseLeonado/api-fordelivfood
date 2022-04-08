48e3da5a8a7bf045c533e3488e4668235906eda8
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2022-04-07 22:57:00 -0300

    Consultando objetos do banco de dados

---
+++ src/main/java/com/jlcb/fordelivfood/jpa/CadastroCozinha.java
@@ -0,0 +1,23 @@
+package com.jlcb.fordelivfood.jpa;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Component;
+
+import com.jlcb.fordelivfood.domain.model.Cozinha;
+
+@Component
+public class CadastroCozinha {
+
+	@PersistenceContext
+	private EntityManager entityManager;
+	
+	public List<Cozinha> listar() {
+		return entityManager.createQuery("FROM Cozinha", Cozinha.class)
+				.getResultList();
+	}
+	
+}
