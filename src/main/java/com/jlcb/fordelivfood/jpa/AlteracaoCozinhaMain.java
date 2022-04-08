b82c8f2cb11627573506795caca37311521b75ec
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2022-04-07 23:13:32 -0300

    Atualizando um objeto no banco de dados

--- src/main/java/com/jlcb/fordelivfood/jpa/AlteracaoCozinhaMain.java
+++ src/main/java/com/jlcb/fordelivfood/jpa/AlteracaoCozinhaMain.java
@@ -7,17 +7,20 @@ import org.springframework.context.ApplicationContext;
 import com.jlcb.fordelivfood.ApiFordelivfoodApplication;
 import com.jlcb.fordelivfood.domain.model.Cozinha;
 
-public class BuscaCozinhaMain2 {
+public class AlteracaoCozinhaMain {
 
 	public static void main(String[] args) {
 		ApplicationContext applicationContext = new SpringApplicationBuilder(ApiFordelivfoodApplication.class)
-				.web(WebApplicationType.NONE).run(args);
-
+				.web(WebApplicationType.NONE)
+				.run(args);
+		
 		CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
-
-		Cozinha cozinha = cadastroCozinha.buscar(1L);
-
-		System.out.println(cozinha.getNome());
+		
+		Cozinha cozinha = new Cozinha();
+		cozinha.setId(1L);
+		cozinha.setNome("Brasileira");
+		
+		cadastroCozinha.adicionar(cozinha);
 	}
-
+	
 }
--- src/main/java/com/jlcb/fordelivfood/jpa/BuscaCozinhaMain.java
+++ src/main/java/com/jlcb/fordelivfood/jpa/BuscaCozinhaMain.java
@@ -7,26 +7,17 @@ import org.springframework.context.ApplicationContext;
 import com.jlcb.fordelivfood.ApiFordelivfoodApplication;
 import com.jlcb.fordelivfood.domain.model.Cozinha;
 
-public class InclsuaoCozinhaMain2 {
+public class BuscaCozinhaMain {
 
 	public static void main(String[] args) {
 		ApplicationContext applicationContext = new SpringApplicationBuilder(ApiFordelivfoodApplication.class)
-				.web(WebApplicationType.NONE)
-				.run(args);
-		
+				.web(WebApplicationType.NONE).run(args);
+
 		CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
-		
-		Cozinha cozinha1 = new Cozinha();
-		cozinha1.setNome("Brasileira");
-		
-		Cozinha cozinha2 = new Cozinha();
-		cozinha2.setNome("Japonesa");
-		
-		cozinha1 = cadastroCozinha.adicionar(cozinha1);
-		cozinha2 = cadastroCozinha.adicionar(cozinha2);
-		
-		System.out.printf("%d - %s\n", cozinha1.getId(), cozinha1.getNome());
-		System.out.printf("%d - %s\n", cozinha2.getId(), cozinha2.getNome());
+
+		Cozinha cozinha = cadastroCozinha.buscar(1L);
+
+		System.out.println(cozinha.getNome());
 	}
-	
+
 }
