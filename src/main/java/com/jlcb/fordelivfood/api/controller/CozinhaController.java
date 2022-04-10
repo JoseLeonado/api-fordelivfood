797afc1acab1f9a86e9d63973552ef873ea4d2c6
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2022-04-10 05:32:42 -0300

    Refatorando a exclusão de cozinhas para usar domain services

--- src/main/java/com/jlcb/fordelivfood/api/controller/CozinhaController.java
+++ src/main/java/com/jlcb/fordelivfood/api/controller/CozinhaController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +18,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jlcb.fordelivfood.api.model.CozinhasXmlWrapper;
+import com.jlcb.fordelivfood.domain.exception.EntidadeEmUsoException;
+import com.jlcb.fordelivfood.domain.exception.EntidadeNaoEncontradaException;
 import com.jlcb.fordelivfood.domain.model.Cozinha;
 import com.jlcb.fordelivfood.domain.repository.CozinhaRepository;
 import com.jlcb.fordelivfood.domain.service.CadastroCozinhaService;
@@ -29,7 +30,7 @@ public class CozinhaController {
 
 	@Autowired
 	private CozinhaRepository cozinhaRepository;
-	
+
 	@Autowired
 	private CadastroCozinhaService cadastroCozinha;
 
@@ -74,19 +75,15 @@ public class CozinhaController {
 
 		return ResponseEntity.notFound().build();
 	}
-	
+
 	@DeleteMapping("{cozinhaId}")
 	public ResponseEntity<Cozinha> remover(@PathVariable Long cozinhaId) {
 		try {
-			Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
-
-			if (cozinha != null) {
-				cozinhaRepository.remover(cozinha);
-				return ResponseEntity.noContent().build();
-			}
-			
+			cadastroCozinha.excluir(cozinhaId);
+			return ResponseEntity.noContent().build();
+		} catch (EntidadeNaoEncontradaException e) {
 			return ResponseEntity.notFound().build();
-		} catch (DataIntegrityViolationException e) {
+		} catch (EntidadeEmUsoException e) {
 			return ResponseEntity.status(HttpStatus.CONFLICT).build();
 		}
 	}
---
+++ src/main/java/com/jlcb/fordelivfood/domain/exception/EntidadeNaoEncontradaException.java
@@ -0,0 +1,11 @@
+package com.jlcb.fordelivfood.domain.exception;
+
+public class EntidadeNaoEncontradaException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public EntidadeNaoEncontradaException(String mensagem) {
+		super(mensagem);
+	}
+
+}
