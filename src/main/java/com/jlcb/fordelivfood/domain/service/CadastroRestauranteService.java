cd3e7cac8fb6a51b56cc4cf39d5f94b4c2eb61ae
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2022-04-10 17:37:55 -0300

    Modelando e implementando a inclusão de recursos de restaurantes

---
+++ src/main/java/com/jlcb/fordelivfood/domain/service/CadastroRestauranteService.java
@@ -0,0 +1,35 @@
+package com.jlcb.fordelivfood.domain.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jlcb.fordelivfood.domain.exception.EntidadeNaoEncontradaException;
+import com.jlcb.fordelivfood.domain.model.Cozinha;
+import com.jlcb.fordelivfood.domain.model.Restaurante;
+import com.jlcb.fordelivfood.domain.repository.CozinhaRepository;
+import com.jlcb.fordelivfood.domain.repository.RestauranteRepository;
+
+@Service
+public class CadastroRestauranteService {
+
+	@Autowired
+	private RestauranteRepository restauranteRepository;
+
+	@Autowired
+	private CozinhaRepository cozinhaRepository;
+
+	public Restaurante salvar(Restaurante restaurante) {
+		Long cozinhaId = restaurante.getCozinha().getId();
+		Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
+
+		if (cozinha == null) {
+			throw new EntidadeNaoEncontradaException(
+					String.format("Não existe cadastro de cozinha com código %d", cozinhaId));
+		}
+
+		restaurante.setCozinha(cozinha);
+
+		return restauranteRepository.salvar(restaurante);
+	}
+
+}
