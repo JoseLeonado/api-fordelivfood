e9adf12e9516b7a7c2c8fa4d6da9a251ec684598
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2022-04-10 00:31:50 -0300

    Modelando e implementando a atualização de recursos com PUT

--- src/main/java/com/jlcb/fordelivfood/api/controller/CozinhaController.java
+++ src/main/java/com/jlcb/fordelivfood/api/controller/CozinhaController.java
@@ -2,6 +2,7 @@ package com.jlcb.fordelivfood.api.controller;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -9,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -38,12 +40,12 @@ public class CozinhaController {
 	@GetMapping("/{cozinhaId}")
 	public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
 		Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
-		
+
 		if (cozinha != null) {
-			return ResponseEntity.ok(cozinha);			
+			return ResponseEntity.ok(cozinha);
 		}
-		
-		return ResponseEntity.notFound().build(); 
+
+		return ResponseEntity.notFound().build();
 	}
 
 	@PostMapping
@@ -51,5 +53,20 @@ public class CozinhaController {
 	public Cozinha adicionar(@RequestBody Cozinha cozinha) {
 		return cozinhaRepository.salvar(cozinha);
 	}
-	
+
+	@PutMapping("{cozinhaId}")
+	public ResponseEntity<Cozinha> atualizar(@PathVariable Long cozinhaId, @RequestBody Cozinha cozinha) {
+		Cozinha cozinhaAtual = cozinhaRepository.buscar(cozinhaId);
+
+		if (cozinhaAtual != null) {
+//			cozinhaAtual.setNome(cozinha.getNome());
+			BeanUtils.copyProperties(cozinha, cozinhaAtual, "id");
+
+			cozinhaAtual = cozinhaRepository.salvar(cozinhaAtual);
+			return ResponseEntity.ok(cozinhaAtual);
+		}
+
+		return ResponseEntity.notFound().build();
+	}
+
 }
