f4f97efb5d25e6afb12b3c360bcd4e03dcfb5ad9
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2022-04-11 22:36:26 -0300

    Implementando consultas JPQL em repositórios

--- src/main/java/com/jlcb/fordelivfood/infrastructure/repository/CozinhaRepositoryImpl.java
+++ src/main/java/com/jlcb/fordelivfood/infrastructure/repository/CozinhaRepositoryImpl.java
@@ -6,13 +6,13 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.jlcb.fordelivfood.domain.model.Cozinha;
 import com.jlcb.fordelivfood.domain.repository.CozinhaRepository;
 
-@Component
+@Repository
 public class CozinhaRepositoryImpl implements CozinhaRepository {
 
 	@PersistenceContext
@@ -21,6 +21,13 @@ public class CozinhaRepositoryImpl implements CozinhaRepository {
 	@Override
 	public List<Cozinha> listar() {
 		return entityManager.createQuery("FROM Cozinha", Cozinha.class).getResultList();
+	}
+	
+	@Override
+	public List<Cozinha> consultarPorNome(String nome) {
+		return entityManager.createQuery("FROM Cozinha WHERE nome LIKE :nome", Cozinha.class)
+				.setParameter("nome", "%" + nome + "%")
+				.getResultList();
 	}
 	
 	@Override
