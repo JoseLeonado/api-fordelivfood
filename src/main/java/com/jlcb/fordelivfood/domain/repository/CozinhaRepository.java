89502c0b0b4b207182d59fa47109a1ff31e5fcf1
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2022-04-13 23:13:00 -0300

    Criando consultas com query methods

--- src/main/java/com/jlcb/fordelivfood/domain/repository/CozinhaRepository.java
+++ src/main/java/com/jlcb/fordelivfood/domain/repository/CozinhaRepository.java
@@ -1,5 +1,8 @@
 package com.jlcb.fordelivfood.domain.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +11,8 @@ import com.jlcb.fordelivfood.domain.model.Cozinha;
 @Repository
 public interface CozinhaRepository extends JpaRepository<Cozinha, Long> {
 	
-//	List<Cozinha> consultarPorNome(String nome);
+	List<Cozinha> findTodasByNome(String nome);
 
+	Optional<Cozinha> findByNome(String nome);
+	
 }
