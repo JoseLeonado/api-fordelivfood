3b932deec9a9659d68f7121ee5d06072e8fcda9c
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2022-04-16 03:50:53 -0300

    Conhecendo os prefixos de query methods

--- src/main/java/com/jlcb/fordelivfood/api/controller/TesteController.java
+++ src/main/java/com/jlcb/fordelivfood/api/controller/TesteController.java
@@ -30,18 +30,38 @@ public class TesteController {
 	}
 	
 	@GetMapping("/cozinhas/unica-por-nome")
-	public Optional<Cozinha> cozinhasUnicaPorNome(String nome) {
+	public Optional<Cozinha> cozinhaUnicaPorNome(String nome) {
 		return cozinhaRepository.findByNome(nome);
+	}
+	
+	@GetMapping("/cozinhas/exists-por-nome")
+	public boolean cozinhaExistsPorNome(String nome) {
+		return cozinhaRepository.existsByNome(nome);
 	}
 	
 	@GetMapping("/restaurantes/por-taxa-frete")
 	public List<Restaurante> restaurantesPorTaxaFrete(BigDecimal taxaInicial, BigDecimal taxaFinal) {
-		return restauranteRepository.findByTaxaFreteBetween(taxaInicial, taxaFinal);
+		return restauranteRepository.queryByTaxaFreteBetween(taxaInicial, taxaFinal);
 	}
 	
 	@GetMapping("/restaurantes/por-nome")
 	public List<Restaurante> restaurantesPorNome(String nome, Long cozinhaId) {
 		return restauranteRepository.findByNomeContainingAndCozinhaId(nome, cozinhaId);
+	}
+	
+	@GetMapping("/restaurantes/primeiro-por-nome")
+	public Optional<Restaurante> restaurantePrimeiroPorNome(String nome) {
+		return restauranteRepository.findFirstRestauranteByNomeContaining(nome);
+	}
+	
+	@GetMapping("/restaurantes/top2-por-nome")
+	public List<Restaurante> restaurantesTop2PorNome(String nome) {
+		return restauranteRepository.findTop2ByNomeContaining(nome);
+	}
+	
+	@GetMapping("/restaurantes/count-por-cozinha")
+	public int restaurantesCountPorCozinha(Long cozinhaId) {
+		return restauranteRepository.countByCozinhaId(cozinhaId);
 	}
 	
 }
