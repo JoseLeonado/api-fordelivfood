d26db27efcf8900635ac7ab5da2bf10a21582c28
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2022-04-18 22:49:58 -0300

    Implementando um repositório Spring Data JPA customizado

---
+++ src/main/java/com/jlcb/fordelivfood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -0,0 +1,32 @@
+package com.jlcb.fordelivfood.infrastructure.repository;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Repository;
+
+import com.jlcb.fordelivfood.domain.model.Restaurante;
+import com.jlcb.fordelivfood.domain.repository.RestauranteRepositoryQueries;
+
+@Repository
+public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
+
+	@PersistenceContext
+	private EntityManager entityManager;
+	
+	@Override
+	public List<Restaurante> find(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
+		
+		String jpql = "FROM Restaurante WHERE nome LIKE :nome AND taxaFrete BETWEEN :taxaInicial AND :taxaFinal";
+		
+		return entityManager.createQuery(jpql, Restaurante.class)
+				.setParameter("nome", "%" + nome + "%")
+				.setParameter("taxaInicial", taxaFreteInicial)
+				.setParameter("taxaFinal", taxaFreteFinal)
+				.getResultList();
+	}
+	
+}
