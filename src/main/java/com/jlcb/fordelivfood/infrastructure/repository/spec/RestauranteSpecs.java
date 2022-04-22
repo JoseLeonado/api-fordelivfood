bab55cfbb5d9fec5cfe5a4b6a24930d36d2c8625
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2022-04-22 17:24:30 -0300

    Criando uma fábrica de Specifications

---
+++ src/main/java/com/jlcb/fordelivfood/infrastructure/repository/spec/RestauranteSpecs.java
@@ -0,0 +1,19 @@
+package com.jlcb.fordelivfood.infrastructure.repository.spec;
+
+import java.math.BigDecimal;
+
+import org.springframework.data.jpa.domain.Specification;
+
+import com.jlcb.fordelivfood.domain.model.Restaurante;
+
+public class RestauranteSpecs {
+
+	public static Specification<Restaurante> comFreteGratis() {
+		return (root, quey, builder) -> builder.equal(root.get("taxaFrete"), BigDecimal.ZERO);
+	}
+
+	public static Specification<Restaurante> comNomeSemelhante(String nome) {
+		return (root, quey, builder) -> builder.like(root.get("nome"), "%" + nome + "%");
+	}
+
+}
