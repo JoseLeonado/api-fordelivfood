b109737a5614863321fc5f4d280ab3e3582d7cee
José Leonardo Cordeiro Bahia <leonardo_cordeirobahia@hotmail.com>
2022-06-23 21:01:18 -0300

    Estendendo ResponseStatusException

--- src/main/java/com/jlcb/fordelivfood/api/controller/CozinhaController.java
+++ src/main/java/com/jlcb/fordelivfood/api/controller/CozinhaController.java
@@ -16,9 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
-import com.jlcb.fordelivfood.domain.exception.EntidadeNaoEncontradaException;
 import com.jlcb.fordelivfood.domain.model.Cozinha;
 import com.jlcb.fordelivfood.domain.repository.CozinhaRepository;
 import com.jlcb.fordelivfood.domain.service.CadastroCozinhaService;
@@ -85,13 +83,7 @@ public class CozinhaController {
 	@DeleteMapping("{cozinhaId}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void remover(@PathVariable Long cozinhaId) {
-		try {
-			cadastroCozinha.excluir(cozinhaId);
-		} catch (EntidadeNaoEncontradaException e) {
-			throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
-//			throw new ServerWebInputException(e.getMessage());
-		}
-		
+		cadastroCozinha.excluir(cozinhaId);
 	}
 
 }
--- src/main/java/com/jlcb/fordelivfood/domain/service/CadastroCozinhaService.java
+++ src/main/java/com/jlcb/fordelivfood/domain/service/CadastroCozinhaService.java
@@ -3,9 +3,7 @@ package com.jlcb.fordelivfood.domain.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
 import com.jlcb.fordelivfood.domain.exception.EntidadeEmUsoException;
 import com.jlcb.fordelivfood.domain.exception.EntidadeNaoEncontradaException;
@@ -25,11 +23,9 @@ public class CadastroCozinhaService {
 	public void excluir(Long cozinhaId) {
 		try {
 			cozinhaRepository.deleteById(cozinhaId);
-		} catch (EmptyResultDataAccessException e) {
-			throw new ResponseStatusException(HttpStatus.NOT_FOUND, 
+		} catch (EmptyResultDataAccessException e) {			
+			throw new EntidadeNaoEncontradaException(
 					String.format("Não existe um cadastro de cozinha com código %d", cozinhaId));
-//			throw new EntidadeNaoEncontradaException(
-//					String.format("Não existe um cadastro de cozinha com código %d", cozinhaId));
 		} catch (DataIntegrityViolationException e) {
 			throw new EntidadeEmUsoException(String.format("Cozinha de código %d não pode ser removida, pois está em uso", cozinhaId));
 		}
