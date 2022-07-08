5d0ae1b0a05db90e19dd080b31fdcb122064f3fd
José Leonardo Cordeiro Bahia <leonardo_cordeirobahia@hotmail.com>
2022-07-08 11:43:00 -0300

    Criando um exception handler global com ResponseEntityExceptionHandler

--- src/main/java/com/jlcb/fordelivfood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/jlcb/fordelivfood/api/exceptionhandler/ApiExceptionHandler.java
@@ -7,13 +7,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import com.jlcb.fordelivfood.domain.exception.EntidadeEmUsoException;
 import com.jlcb.fordelivfood.domain.exception.EntidadeNaoEncontradaException;
 import com.jlcb.fordelivfood.domain.exception.NegocioException;
 
 @ControllerAdvice
-public class ApiExceptionHandler {
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
 	@ExceptionHandler(EntidadeNaoEncontradaException.class)
 	public ResponseEntity<?> tratarEntidadeNaoEncontradaException(EntidadeNaoEncontradaException e) {
@@ -43,16 +43,6 @@ public class ApiExceptionHandler {
 		
 		return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
 				.body(problema);
-	}
-	
-	@ExceptionHandler(EntidadeEmUsoException.class)
-	public ResponseEntity<?> tratarEntidadeEmUsoException(EntidadeEmUsoException e) {
-	    Problema problema = Problema.builder()
-	            .dataHora(LocalDateTime.now())
-	            .mensagem(e.getMessage()).build();
-	    
-	    return ResponseEntity.status(HttpStatus.CONFLICT)
-	            .body(problema);
 	}
 
 }
