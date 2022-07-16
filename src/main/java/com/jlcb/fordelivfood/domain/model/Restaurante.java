cb050aef02018487faafb410564058a0804c0d91
José Leonardo Cordeiro Bahia <leonardo_cordeirobahia@hotmail.com>
2022-07-16 14:09:41 -0300

    Criando constraints de validação customizadas com implementação de ConstraintValidator

--- src/main/java/com/jlcb/fordelivfood/domain/model/Restaurante.java
+++ src/main/java/com/jlcb/fordelivfood/domain/model/Restaurante.java
@@ -19,6 +19,7 @@ import javax.persistence.OneToMany;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PositiveOrZero;
 import javax.validation.groups.ConvertGroup;
 import javax.validation.groups.Default;
 
@@ -27,7 +28,7 @@ import org.hibernate.annotations.UpdateTimestamp;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.jlcb.fordelivfood.core.validation.Groups;
-import com.jlcb.fordelivfood.core.validation.TaxaFrete;
+import com.jlcb.fordelivfood.core.validation.Multiplo;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -47,8 +48,8 @@ public class Restaurante {
 	private String nome;
 
 	@NotNull
-//	@PositiveOrZero
-	@TaxaFrete
+	@PositiveOrZero
+	@Multiplo(numero = 5)
 	@Column(nullable = false)
 	private BigDecimal taxaFrete;
 	
