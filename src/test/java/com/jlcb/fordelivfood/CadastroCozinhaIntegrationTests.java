6ab6d684eab2bf1bb2239557058bb463c10dc1dc
José Leonardo Cordeiro Bahia <leonardo_cordeirobahia@hotmail.com>
2022-07-16 18:44:06 -0300

    Criando e rodando um teste de integração com Spring Boot, JUnit e AssertJ

---
+++ src/test/java/com/jlcb/fordelivfood/CadastroCozinhaIntegrationTests.java
@@ -0,0 +1,47 @@
+package com.jlcb.fordelivfood;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import javax.validation.ConstraintViolationException;
+
+import com.jlcb.fordelivfood.domain.model.Cozinha;
+import com.jlcb.fordelivfood.domain.service.CadastroCozinhaService;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class CadastroCozinhaIntegrationTests {
+	
+	@Autowired
+	private CadastroCozinhaService service;
+
+	@Test
+	public void deveAtribuirId_QuandoCadastrarCozinhaComDadosCorretos() {
+		Cozinha novaCozinha = new Cozinha();
+		novaCozinha.setNome("Chinesa");
+		
+		novaCozinha = service.salvar(novaCozinha);
+		
+		assertThat(novaCozinha).isNotNull();
+		assertThat(novaCozinha.getId()).isNotNull();
+	}
+	
+	@Test
+	public void deveFalhar_QuandoCadastrarCozinhaSemNome() {
+		Cozinha novaCozinha = new Cozinha();
+		novaCozinha.setNome(null);
+
+		ConstraintViolationException erroEsperado =
+				Assertions.assertThrows(ConstraintViolationException.class, () -> {
+					service.salvar(novaCozinha);
+				});
+
+		assertThat(erroEsperado).isNotNull();
+	}
+}
