ce0517b373f77facc102d5a2692ae34a57ce1761
José Leonardo Cordeiro Bahia <leonardo_cordeirobahia@hotmail.com>
2022-07-17 10:57:25 -0300

    Voltando o estado inicial do banco de dados para cada execução de teste com callback do Flyway

--- src/test/java/com/jlcb/fordelivfood/CadastroCozinhaIT.java
+++ src/test/java/com/jlcb/fordelivfood/CadastroCozinhaIT.java
@@ -3,9 +3,11 @@ package com.jlcb.fordelivfood;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.hasSize;
 
+import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
@@ -21,11 +23,16 @@ class CadastroCozinhaIT {
 	@LocalServerPort
 	private int port;
 	
+	@Autowired
+	private Flyway flyway;
+	
 	@BeforeEach
 	public void setUp() {
 		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
 		RestAssured.port = port;
 		RestAssured.basePath = "/cozinhas";
+		
+		flyway.migrate();
 	}
 
 	@Test
@@ -46,6 +53,18 @@ class CadastroCozinhaIT {
 			.get()
 		.then()
 			.body("", hasSize(4));
+	}
+	
+	@Test
+	public void testRetornarStatus201_QuandoCadastrarCozinha() {
+		given()
+			.body("{ \"nome\": \"Chinesa\" }")
+			.contentType(ContentType.JSON)
+			.accept(ContentType.JSON)
+		.when()
+			.post()
+		.then()
+			.statusCode(HttpStatus.CREATED.value());
 	}
 
 }
