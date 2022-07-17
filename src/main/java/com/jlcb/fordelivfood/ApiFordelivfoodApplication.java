ee9dc4fd7e87cf447c9fa910e10e1efc482ca89c
José Leonardo Cordeiro Bahia <leonardo_cordeirobahia@hotmail.com>
2022-07-17 14:48:13 -0300

    Configurando e refatorando o projeto para usar UTC

--- src/main/java/com/jlcb/fordelivfood/ApiFordelivfoodApplication.java
+++ src/main/java/com/jlcb/fordelivfood/ApiFordelivfoodApplication.java
@@ -1,5 +1,7 @@
 package com.jlcb.fordelivfood;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -11,6 +13,7 @@ import com.jlcb.fordelivfood.infrastructure.repository.CustomJpaRepositoryImpl;
 public class ApiFordelivfoodApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 		SpringApplication.run(ApiFordelivfoodApplication.class, args);
 	}
 
