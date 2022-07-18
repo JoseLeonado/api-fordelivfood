67da443a6830ddb1fea010748acee97d2b2fd55f
José Leonardo Cordeiro Bahia <leonardo_cordeirobahia@hotmail.com>
2022-07-18 19:54:11 -0300

    Refatorando e criando um assembler de DTO

--- src/main/java/com/jlcb/fordelivfood/core/jackson/JacksonMixinModule.java
+++ src/main/java/com/jlcb/fordelivfood/core/jackson/JacksonMixinModule.java
@@ -5,10 +5,8 @@ import org.springframework.stereotype.Component;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.jlcb.fordelivfood.api.model.mixin.CidadeMixin;
 import com.jlcb.fordelivfood.api.model.mixin.CozinhaMixin;
-import com.jlcb.fordelivfood.api.model.mixin.RestauranteMixin;
 import com.jlcb.fordelivfood.domain.model.Cidade;
 import com.jlcb.fordelivfood.domain.model.Cozinha;
-import com.jlcb.fordelivfood.domain.model.Restaurante;
 
 @Component
 public class JacksonMixinModule extends SimpleModule {
@@ -16,7 +14,6 @@ public class JacksonMixinModule extends SimpleModule {
 	private static final long serialVersionUID = 1L;
 	
 	public JacksonMixinModule() {
-		setMixInAnnotation(Restaurante.class, RestauranteMixin.class);
 		setMixInAnnotation(Cidade.class, CidadeMixin.class);
 		setMixInAnnotation(Cozinha.class, CozinhaMixin.class);
 	}
