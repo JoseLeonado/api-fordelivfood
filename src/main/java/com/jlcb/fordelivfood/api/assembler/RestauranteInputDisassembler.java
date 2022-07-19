9b8376cbed57a446cc4b57d54cc54faeebe34ab5
José Leonardo Cordeiro Bahia <leonardo_cordeirobahia@hotmail.com>
2022-07-19 20:50:12 -0300

    Mapeando para uma instância destino (e não um tipo) com ModelMapper

--- src/main/java/com/jlcb/fordelivfood/api/assembler/RestauranteInputDisassembler.java
+++ src/main/java/com/jlcb/fordelivfood/api/assembler/RestauranteInputDisassembler.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.jlcb.fordelivfood.api.model.input.RestauranteInput;
+import com.jlcb.fordelivfood.domain.model.Cozinha;
 import com.jlcb.fordelivfood.domain.model.Restaurante;
 
 @Component
@@ -15,6 +16,13 @@ public class RestauranteInputDisassembler {
 
 	public Restaurante toDomainObject(RestauranteInput restauranteInput) {
 		return modelMapper.map(restauranteInput, Restaurante.class);
+	}
+	
+	public void copyToDomainObject(RestauranteInput restauranteInput, Restaurante restaurante) {
+		// Para evitar org.hibernate.HibernateException: identifier of an instance of 
+		// com.jlcb.fordelivfood.domain.model.Cozinha was altered from 1 to 2
+		restaurante.setCozinha(new Cozinha());
+		modelMapper.map(restauranteInput, restaurante);
 	}
 	
 }
