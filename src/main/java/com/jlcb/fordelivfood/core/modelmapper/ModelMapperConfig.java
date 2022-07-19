a62ab22744eeda77506464cf2d540859a836218e
José Leonardo Cordeiro Bahia <leonardo_cordeirobahia@hotmail.com>
2022-07-19 20:40:28 -0300

    Customizando o mapeamento de propriedades com ModelMapper

--- src/main/java/com/jlcb/fordelivfood/core/modelmapper/ModelMapperConfig.java
+++ src/main/java/com/jlcb/fordelivfood/core/modelmapper/ModelMapperConfig.java
@@ -4,12 +4,20 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.jlcb.fordelivfood.api.model.RestauranteDTO;
+import com.jlcb.fordelivfood.domain.model.Restaurante;
+
 @Configuration
 public class ModelMapperConfig {
 
 	@Bean
 	public ModelMapper modelMapper() {
-		return new ModelMapper();
+		ModelMapper modelMapper = new ModelMapper();
+		
+		modelMapper.createTypeMap(Restaurante.class, RestauranteDTO.class)
+			.addMapping(Restaurante::getTaxaFrete, RestauranteDTO::setPrecoFrete);
+		
+		return modelMapper;
 	}
 	
 }
