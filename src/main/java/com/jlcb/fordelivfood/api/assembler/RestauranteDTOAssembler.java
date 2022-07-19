d7070fb2e50d54d76b166a6c6a54d8927fb0a8ac
José Leonardo Cordeiro Bahia <leonardo_cordeirobahia@hotmail.com>
2022-07-19 20:22:44 -0300

    Adicionando e usando o ModelMapper

--- src/main/java/com/jlcb/fordelivfood/api/assembler/RestauranteDTOAssembler.java
+++ src/main/java/com/jlcb/fordelivfood/api/assembler/RestauranteDTOAssembler.java
@@ -3,26 +3,21 @@ package com.jlcb.fordelivfood.api.assembler;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.jlcb.fordelivfood.api.model.CozinhaDTO;
 import com.jlcb.fordelivfood.api.model.RestauranteDTO;
 import com.jlcb.fordelivfood.domain.model.Restaurante;
 
 @Component
 public class RestauranteDTOAssembler {
+	
+	@Autowired
+	private ModelMapper modelMapper;
 
 	public RestauranteDTO toDTO(Restaurante restaurante) {
-		CozinhaDTO cozinhaDTO = new CozinhaDTO();
-		cozinhaDTO.setId(restaurante.getCozinha().getId());
-		cozinhaDTO.setNome(restaurante.getCozinha().getNome());
-		
-		RestauranteDTO restauranteDTO = new RestauranteDTO();
-		restauranteDTO.setId(restaurante.getId());
-		restauranteDTO.setNome(restaurante.getNome());
-		restauranteDTO.setTaxaFrete(restaurante.getTaxaFrete());
-		restauranteDTO.setCozinha(cozinhaDTO);
-		return restauranteDTO;
+		return modelMapper.map(restaurante, RestauranteDTO.class);
 	}
 	
 	public List<RestauranteDTO> toCollectionDTO(List<Restaurante> restaurantes) {
