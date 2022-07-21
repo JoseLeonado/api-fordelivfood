649488724cab5a5b913a10977b574215700551ce
José Leonardo Cordeiro Bahia <leonardo_cordeirobahia@hotmail.com>
2022-07-21 15:19:45 -0300

    Adicionando endereço no modelo da representação do recurso de restaurante

--- src/main/java/com/jlcb/fordelivfood/core/modelmapper/ModelMapperConfig.java
+++ src/main/java/com/jlcb/fordelivfood/core/modelmapper/ModelMapperConfig.java
@@ -1,15 +1,27 @@
 package com.jlcb.fordelivfood.core.modelmapper;
 
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeMap;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.jlcb.fordelivfood.api.model.EnderecoDTO;
+import com.jlcb.fordelivfood.domain.model.Endereco;
 
 @Configuration
 public class ModelMapperConfig {
 
 	@Bean
 	public ModelMapper modelMapper() {
-		return new ModelMapper();
+		ModelMapper modelMapper = new ModelMapper();
+		
+		TypeMap<Endereco, EnderecoDTO> enderecoToEnderecoModelTypeMap = modelMapper.createTypeMap(Endereco.class, EnderecoDTO.class);
+		
+		enderecoToEnderecoModelTypeMap.<String>addMapping(
+				enderecoSrc -> enderecoSrc.getCidade().getEstado().getNome(), 
+				(enderecoDTODestino, value) -> enderecoDTODestino.getCidade().setEstado(value));
+		
+		return modelMapper;
 	}
 	
 }
